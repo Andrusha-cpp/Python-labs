0bed91633d13890601141112ad047a6f472754b2
Andrusha-cpp <karabanov.andr@gmail.com>
2025-09-30 02:21:34 +0300

    Add f-strings to output

--- Task12.py
+++ Task12.py
@@ -10,16 +10,16 @@ print("Base Tariff:", payment)
 if internet > 1000:
     add_internet = internet - 1000
     payment += add_internet * 0.79
-    print("You have extra", add_internet, "megabytes: +", round(add_internet, 2) * 0.79, "rubles")
+    print(f"You have extra {add_internet} megabytes: + {round(add_internet, 2) * 0.79} rubles")
 if minutes > 60:
     add_minutes = minutes - 60
     payment += add_minutes * 0.89
-    print("You have extra", add_minutes, "minutes: +", round(add_minutes, 2) * 0.89, "rubles")
+    print(f"You have extra {add_minutes} minutes: + {round(add_minutes, 2) * 0.89} rubles")
 if sms > 30:
     add_sms = sms - 30
     payment += add_sms * 0.59
-    print("You have extra", add_sms, "messages: +", round(add_sms * 0.59, 2), "rubles")
+    print(f"You have extra {add_sms} messages: + {round(add_sms * 0.59, 2)} rubles")
 
 print("Tax: 2%")
 payment = round(payment * tax, 2)
-print("Total payment:", payment)
+print(f"Total payment: {payment}")
