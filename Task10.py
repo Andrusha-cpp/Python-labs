885da56bcf68d4862353252a6ca2564d776ab1f7
Andrusha-cpp <karabanov.andr@gmail.com>
2025-09-30 02:20:28 +0300

    Make output more understandable

--- Task10.py
+++ Task10.py
@@ -5,5 +5,5 @@ print("a + b =", a + b,
       "\na - b =", a - b,
       "\na * b =", a * b,
       "\na / b =", a / b,
-      "\na % b =", a % b,
-      "\na ^ b =", a ** b)
+      "\nremainder a from b =", a % b,
+      "\na to the power of b =", a ** b)
