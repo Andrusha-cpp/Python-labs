d2b414928c2f2498541671e0981b700f0ccc9f57
Andrusha-cpp <karabanov.andr@gmail.com>
2025-09-30 02:15:34 +0300

    Rewrite to conditions

--- Task11.py
+++ Task11.py
@@ -1,77 +1,70 @@
-date_str = input("Enter date of birthday (example: 31.01.2024):")
+print("Enter date of birthday:")
+year = int(input("Year: "))
+month = int(input("Month: "))
+day = int(input("Day: "))
 
-try:
-    day, month, year = map(int, date_str.split(".")) #check that data has only numbers and points
-    print(day, month, year) 
-except ValueError:
-    print("Incorrect data. Restart programm and try again.")
-    exit() 
-
-if (day or month or year) <= 0 or day > 31 or month > 12: #check that data have correct numbers 
-     print("Incorrect data. Restart programm and try again.")
-     exit()
+print(day, month, year) 
 
 print("Zodiac sign is", end=" ") #selection of zodiac sign 
-match month:
-    case 1:
-        if day < 20:
-            print("Goat")
-        else:
-            print("Water Bearer")
-    case 2:
-        if day < 19:
-            print("Water Bearer")
-        else:
-            print("Fish")
-    case 3:
-        if day < 21:
-            print("Fish")
-        else:
-            print("Ram")
-    case 4:
-        if day < 20:
-            print("Ram")
-        else:
-            print("Bull")
-    case 5:
-        if day < 21:
-            print("Bull")
-        else:
-            print("Twins")
-    case 6:
-        if day < 22:
-            print("Twins")
-        else:
-            print("Crab")
-    case 7:
-        if day < 23:
-            print("Crab")
-        else:
-            print("Lion")
-    case 8:
-        if day < 23:
-            print("Lion")
-        else:
-            print("Virgin")
-    case 9:
-        if day < 23:
-            print("Virgin")
-        else:
-            print("Balance")
-    case 10:
-        if day < 24:
-            print("Balance")
-        else:
-            print("Scorpion")
-    case 11:
-        if day < 22:
-            print("Scorpion")
-        else:
-            print("Archer")
-    case 12:
-        if day < 22:
-            print("Archer")
-        else:
-            print("Goat")
+if month == 1:
+    if day < 20:
+        print("Goat")
+    else:
+        print("Water Bearer")
+elif month == 2:
+    if day < 19:
+        print("Water Bearer")
+    else:
+        print("Fish")
+elif month == 3:
+    if day < 21:
+        print("Fish")
+    else:
+        print("Ram")
+elif month == 4:
+    if day < 20:
+        print("Ram")
+    else:
+        print("Bull")
+elif month == 5:
+    if day < 21:
+        print("Bull")
+    else:
+        print("Twins")
+elif month == 6:
+    if day < 22:
+        print("Twins")
+    else:
+        print("Crab")
+elif month == 7:
+    if day < 23:
+        print("Crab")
+    else:
+        print("Lion")
+elif month == 8:
+    if day < 23:
+        print("Lion")
+    else:
+        print("Virgin")
+elif month == 9:
+    if day < 23:
+        print("Virgin")
+    else:
+        print("Balance")
+elif month == 10:
+    if day < 24:
+        print("Balance")
+    else:
+        print("Scorpion")
+elif month == 11:
+    if day < 22:
+        print("Scorpion")
+    else:
+        print("Archer")
+elif month == 12:
+    if day < 22:
+        print("Archer")
+    else:
+        print("Goat")
 
 
