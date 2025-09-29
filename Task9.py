0acecc60bb9271beabeff9910cb6cb14cd64d310
Andrusha-cpp <karabanov.andr@gmail.com>
2025-09-30 02:26:29 +0300

    Rewrite to conditions

--- Task9.py
+++ Task9.py
@@ -1,14 +1,12 @@
 IP = input("Enter IP-adress: ")
 
-try: #check1
-    num1, num2, num3, num4 = map(int, IP.split("."))
-except ValueError:
+nums = IP.split(".")
+if len(nums) != 4:
     print("Incorrect IP-adress")
-
-try:
-    if all(0 <= n <= 255 for n in (num1, num2, num3, num4)): #check2
+else:
+    for num in nums:
+        num = int(num)
+    if all(0 <= nums <= 255 for num in nums):
         print("Correct IP-adress.")
     else:
         print("Incorrect IP-adress")
-except NameError:
-    print("Incorrect IP-adress")
