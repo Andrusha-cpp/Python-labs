aaf9422fc26f3fae03845ea8adaf61e0532d4065
Andrusha-cpp <karabanov.andr@gmail.com>
2025-10-01 17:57:55 +0300

    First steps

---
+++ Laba3/task_Lab3.py
@@ -0,0 +1,31 @@
+def main():
+    #TODO: some interface
+    pass
+
+class Bank: 
+    def open_account(self):
+        pass
+    def close_account(self):
+        pass
+    def TopUp_account(self):
+        pass
+    def money_transfer(self):
+        pass
+    
+
+class BankAccount:
+    #TODO currency of accaount
+    pass
+
+class Client:
+    def __init__(self, ID):
+        self.ID = ID
+    #TODO: polya: input_ID
+    #TODO somehow make creation of new accounts
+    pass
+
+# >Enter your ID
+# (output interface)
+# add password to ID?
+#
+# at the end of programm ask (continue or finish?)
