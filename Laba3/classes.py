3f6d14c59a34178d85165bc93157314eadd1fd75
Andrusha-cpp <karabanov.andr@gmail.com>
2025-10-12 11:14:41 +0300

    Remove bank and client from paremetres of methods

--- Laba3/classes.py
+++ Laba3/classes.py
@@ -24,27 +24,26 @@ class BankInterface:
     def __init__(self, bank):
         self._bank = bank
 
-    
     #// MAIN_menu вроде готово
-    def MAIN_menu (self, bank): #*def to create or sing in ID 
+    def MAIN_menu (self): #*def to create or sing in ID 
         while True:
 
             action = int(input("Choose action:\n 1.Sing in to ID.\n 2.Create ID"))
             match action:
                 case 1:
-                    self.sign_in(bank) #дальнейший ход интерфейса
+                    self.sign_in(self.bank) #дальнейший ход интерфейса
                     return
                 case 2:
-                    self.registration(bank) #дальнейший ход интерфейса
+                    self.registration(self.bank) #дальнейший ход интерфейса
                     return
                 case _:
                     print("Invalid option. Try again.")
 
     #//тут вроде тоже всё
-    def sign_in(self, bank): 
+    def sign_in(self): 
         while True:
             client_ID = int(input("Enter ID: ")) #?потом string
-            if bank.find_id(client_ID):
+            if self.bank.find_id(client_ID):
                 #дальнейший интерфейс
                 return
             else:
@@ -56,7 +55,7 @@ class BankInterface:
                         case 1:
                             break #back to first cycle
                         case 2:
-                            self.registration(bank)
+                            self.registration(self.bank)
                             print("You've been succesfully registrated!")
                             #?return или сразу переход на (3)
                         case _:
@@ -68,60 +67,63 @@ class BankInterface:
         
 class ClientInterface:
 
-    def MAIN_not_menu(self, client):
+    def __init__(self, client):
+        self.client = client
+
+    def MAIN_not_menu(self):
         while True:
 
             action = int(input("Choose action:\n 1.Open account.\n 2.Close account." \
             "\n 3.Operations with account.\n 4.Get all accounts statement"))
             match action:
                         case 1:
-                            self.openning_interface(client)
+                            self.openning_interface(self.client)
                             print("Account succesfully opened.")
                             return 
                         case 2:
-                            client.close_account()
+                            self.client.close_account()
                             print("Account closed.")
                             return
                         case 3:
-                            self.account_operations(client)
+                            self.account_operations(self.client)
                             return
                         case 4:
-                            client.accounts_statement()
+                            self.client.accounts_statement()
                             print("Something")
                             return
                         case _:
                             print("Invalid option. Try again.")
 
-    def account_operations(self, client):
+    def account_operations(self):
         while True:
 
             action = int(input("Choose action:\n 1.Top up account.\n 2.Withdraw money.\n 3.Transfer money to account."))
             match action:
                         case 1:
-                            client.TopUp_account()
+                            self.client.TopUp_account()
                             return 
                         case 2:
-                            client.withdraw_money()
+                            self.client.withdraw_money()
                             return
                         case 3:
-                            client.money_transfer()
+                            self.client.money_transfer()
                             return
                         case _:
                             print("Invalid option. Try again.")
 
-    def openning_interface(self, client):
+    def openning_interface(self):
         while True:
 
             action = int(input("Choose currency of account:\n 1.USD.\n 2.BYN.\n 3.EUR."))
             match action:
                         case 1:
-                            client.open_account("USD")
+                            self.client.open_account("USD")
                             return 
                         case 2:
-                            client.open_account("BYN")
+                            self.client.open_account("BYN")
                             return
                         case 3:
-                            client.open_account("EUR")
+                            self.client.open_account("EUR")
                             return
                         case _:
                             print("Invalid option. Try again.")
