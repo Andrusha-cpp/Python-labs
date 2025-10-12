491eee312dfbaf9f0ed3d166b643938c88520bcd
Andrusha-cpp <karabanov.andr@gmail.com>
2025-10-12 13:14:49 +0300

    Add methods to ClientInterface and create connection between Client and Account Interfaces

--- Laba3/classes.py
+++ Laba3/classes.py
@@ -28,10 +28,10 @@ class BankInterface:
             action = int(input("Choose action:\n 1.Sing in to ID.\n 2.Create new ID.\n"))
             match action:
                 case 1:
-                    self.sign_in() #дальнейший ход интерфейса
+                    self.sign_in() #дальнейший ход интерфейса внутри
                     return
                 case 2:
-                    self.registration() #дальнейший ход интерфейса
+                    self.registration() #дальнейший ход интерфейса внутри
                     return
                 case _:
                     print("Invalid option. Try again.")
@@ -82,32 +82,21 @@ class ClientInterface:
                             print("Account succesfully opened.")
                             return 
                         case 2:
-                            self.client.close_account()
+                            self.clossing_interface()
                             print("Account closed.")
                             return
                         case 3:
-                            self.account_operations()
-                            return
+                            while True:
+                                acc_number = input("Enter account number: ")
+                                if self.client.find_account(acc_number):
+                                    acc_interface = AccountInterface(acc_number)
+                                    acc_interface.account_operations()
+                                    return
+                                else:
+                                    print("You don't have account with this number. Try again.")
                         case 4:
                             self.client.accounts_statement()
                             print("Something")
-                            return
-                        case _:
-                            print("Invalid option. Try again.")
-
-    def account_operations(self):
-        while True:
-
-            action = int(input("Choose action:\n 1.Top up account.\n 2.Withdraw money.\n 3.Transfer money to account.\n"))
-            match action:
-                        case 1:
-                            self.client.TopUp_account()
-                            return 
-                        case 2:
-                            self.client.withdraw_money()
-                            return
-                        case 3:
-                            self.client.money_transfer()
                             return
                         case _:
                             print("Invalid option. Try again.")
@@ -118,26 +107,70 @@ class ClientInterface:
             action = int(input("Choose currency of account:\n 1.USD.\n 2.BYN.\n 3.EUR.\n"))
             match action:
                         case 1:
-                            self.client.open_account("USD")
+                            print(f"Your account number {self.client.open_account("USD")}")
                             return 
                         case 2:
-                            self.client.open_account("BYN")
+                            print(f"Your account number {self.client.open_account("BYN")}")
                             return
                         case 3:
-                            self.client.open_account("EUR")
+                            print(f"Your account number {self.client.open_account("EUR")}")
+                            return
+                        case _:
+                            print("Invalid option. Try again.")
+
+    def clossing_interface(self):
+        while True:
+            acc_number = input("Enter account number: ") #makedecorator
+            if self.client.find_account(acc_number):
+
+                while True:
+                    action = int(input("Are you sure you want close this account?\n" \
+                                       "1.Yes\n 2.No\n"))
+                    match action:
+                        case 1:
+                            self.client.close_account(acc_number)
+                            return
+                        case 2:
+                            return
+                        case _:
+                            print("Invalid option. Try again.")
+
+            else:
+                print("You don't have account with this number. Try again.")
+
+
+class AccountInterface:
+
+    def __init__(self, account):
+        self.account = account
+
+    def account_operations(self):
+        while True:
+
+            action = int(input("Choose action:\n 1.Top up account.\n 2.Withdraw money.\n 3.Transfer money to account.\n"))
+            match action:
+                        case 1:
+                            self.account.TopUp_account()
+                            return 
+                        case 2:
+                            self.account.withdraw_money()
+                            return
+                        case 3:
+                            self.account.money_transfer()
                             return
                         case _:
                             print("Invalid option. Try again.")
 
 
+
     
 class BankAccount:
 
-    def __init__(self, currency, owner_ID):
+    def __init__(self, currency, owner_ID, account_number):
         self.currency = currency
         self.owner_ID = owner_ID
         self.balance = 0
-        self.account_number = 0
+        self.account_number = account_number
 
     def TopUp_account(self):
         pass
@@ -160,13 +193,18 @@ class Client:
     def get_ID(self):
         return self.ID
 
+    def find_account(self, acc_number):
+        return acc_number in self.accounts
+
     #? get_name
     def open_account(self, currency):
-        self.accounts[self.number_of_accounts] = BankAccount(currency, self.ID)
+        self.accounts[self.number_of_accounts] = BankAccount(currency, self.ID, self.number_of_accounts)
         self.number_of_accounts += 1
+        return self.number_of_accounts - 1
         
-    def close_account(self):
-        #delte from dict
+    def close_account(self, acc_number):
+        del self.accounts[acc_number]
+        print("Account succesfully deleted.")
         pass
 
     def account_statement(self):
