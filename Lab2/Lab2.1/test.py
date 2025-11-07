30c65724313bc3bbd15d11b0cbc4453822065ffa
Andrusha-cpp <karabanov.andr@gmail.com>
2025-11-07 11:18:43 +0300

    delete test.py

--- Lab2/Lab2.1/test.py
+++
@@ -1,5 +0,0 @@
-array = list(map(float, input("Enter the array: ").split()))
-array2 = [float(x) if '.' in x else int(x) for x in input("Enter the array:").split()]
-print(array)
-print(array2)
-print(array2[3] + array2[4])
