b2423c2c79e231caeae7d8e5106db00d78d9cd45
Andrusha-cpp <karabanov.andr@gmail.com>
2025-11-14 00:35:33 +0300

    CHange types of columns

--- Part5/Task.py
+++ Part5/Task.py
@@ -4,8 +4,8 @@ import seaborn as sns
 
 #read from pickle cause it way faster than excel
 df = pd.read_pickle("Part5/cache_table.pkl")
-print(df)
-
 print(df.columns)
+
+print(df.dtypes)
 #df['Profit'] = df['Unnamed: 8'] - df['Unnamed: 9']
 #print(df['Profit'])
